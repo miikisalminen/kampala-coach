f2d3f0ab30852219a89f836ffcd66087b99f99ce
miikisalminen <miika.salminen11@outlook.com>
2022-07-11 09:38:21 +0300

    bugfix

--- bot.py
+++ bot.py
@@ -17,6 +17,8 @@ def write_to_db(author):
     f = open("db.txt", "a")
     f.write("{0}:0\n".format(author))
     f.close
+    members.append([str(author), 0])
+    print(members)
     
 def update_to_db(sum, author):
     for i in members:
@@ -39,11 +41,14 @@ async def on_message(message):
         return
 
     found = False
+    for i in members:
+        if i[0] == str(message.author):
+            found = True
+        
     if message.content =="%register":
         for i in members:
-            if i[0] == str(message.author):
+            if found:
                 response = "You're already in the game champ!"
-                found = True
                 
         if not found:
             response = "Alright {0}! I'm putting you on the list.".format(message.author)
@@ -56,16 +61,16 @@ async def on_message(message):
         
         if message.content.split()[1] =="walk" and found:
             update_to_db(int(message.content.split()[2]),str(message.author))
-            response="Good job, I'm putting down {0} minutes of walking for you. **(+{0} KAP)** :man_walking:".format(message.content.split()[2])
+            response="Good job, I'm putting down {0} minutes of walking for you. **(+{0} <:KAP:995930394715246642>)** :man_walking:".format(message.content.split()[2])
         elif message.content.split()[1] =="run" and found:
             update_to_db(int(message.content.split()[2])*2,str(message.author))
-            response="Good job, I'm putting down {0} minutes of running for you. **(+{1} KAP)** :man_running:".format(message.content.split()[2],int(message.content.split()[2])*2)    
+            response="Good job, I'm putting down {0} minutes of running for you. **(+{1} <:KAP:995930394715246642>)** :man_running:".format(message.content.split()[2],int(message.content.split()[2])*2)    
         elif message.content.split()[1] =="bike" and found:
             update_to_db(int(message.content.split()[2])/2,str(message.author))
-            response="Good job, I'm putting down {0} minutes of biking for you. **(+{1} KAP)** :bike:".format(message.content.split()[2],int(message.content.split()[2])/2)    
+            response="Good job, I'm putting down {0} minutes of biking for you. **(+{1} <:KAP:995930394715246642>)** :bike:".format(message.content.split()[2],int(message.content.split()[2])/2)    
         elif message.content.split()[1] =="gym" and found:
             update_to_db(int(message.content.split()[2])*2,str(message.author))
-            response="Good job, I'm putting down {0} minutes of gym for you. **(+{1} KAP)** :muscle:".format(message.content.split()[2],int(message.content.split()[2])*2)    
+            response="Good job, I'm putting down {0} minutes of gym for you. **(+{1} <:KAP:995930394715246642>)** :muscle:".format(message.content.split()[2],int(message.content.split()[2])*2)    
         else:
             response="We dont do that in this gym... Are you sure you're registered?"
         await message.channel.send(response)
